8eee3163474e7fede9cef0a29a454825d271932b
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-12-17 22:12:54 +0900

    read recipe

--- gqlgen/server.go
+++ gqlgen/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -8,6 +10,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/richardimaoka/kurasiru-clone/gqlgen/graph"
+	"github.com/richardimaoka/kurasiru-clone/gqlgen/graph/model"
 )
 
 const defaultPort = "8080"
@@ -17,6 +20,22 @@ func main() {
 	if port == "" {
 		port = defaultPort
 	}
+
+	filename := "data/recipe1.json"
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		panic("Failed to read: " + filename)
+	}
+	var recipe model.Recipe
+	err = json.Unmarshal(data, &recipe)
+	if err != nil {
+		panic("Failed to unmarshal recipe")
+	}
+	bytes, err := json.Marshal(recipe)
+	if err != nil {
+		panic("Failed to unmarshal recipe")
+	}
+	fmt.Printf("%s", bytes)
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
