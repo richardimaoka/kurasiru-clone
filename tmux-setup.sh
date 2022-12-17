22d55e446b3f1f0bcb708ea4979b28e0905f69ca
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-12-18 03:20:51 +0900

    gqlgen cors

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -16,10 +16,10 @@ tmux select-layout even-vertical   # to avoid 'no space for new pane'
 
 tmux send-keys -t "$SESSION:0.0" '(cd gqlgen && gow -e=gql run github.com/99designs/gqlgen generate)' C-m
 tmux send-keys -t "$SESSION:0.1" '(cd gqlgen && gow -e=go,json run server.go)' C-m
-tmux send-keys -t "$SESSION:0.1" '(cd next && npm run dev)' C-m
+tmux send-keys -t "$SESSION:0.2" '(cd next && npm run dev)' C-m
 
 # open editors
-tmux send-keys -t "$SESSION:0.2" '(cd gqlgen && code .)' C-m
-tmux send-keys -t "$SESSION:0.2" '(cd next && code .)' C-m
+tmux send-keys -t "$SESSION:0.3" '(cd gqlgen && code .)' C-m
+tmux send-keys -t "$SESSION:0.3" '(cd next && code .)' C-m
 
 tmux attach -t "$SESSION"
