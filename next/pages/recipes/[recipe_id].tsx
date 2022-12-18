7c20d76894a26350c9c119bf436f6434ccac908b
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-12-18 11:34:34 +0900

    fetch by recipe id

--- next/pages/recipes/[recipe_id].tsx
+++ next/pages/recipes/[recipe_id].tsx
@@ -2,8 +2,8 @@ import { ApolloClient, gql, InMemoryCache, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 
 const GET_RECIPE = gql`
-  query GetRecipe {
-    recipe(id: "f488a1b0-9a53-4bbb-8c9c-e4e880f998ab") {
+  query GetRecipe($recipeId: ID) {
+    recipe(id: $recipeId) {
       id
       title
       subTitle
@@ -23,7 +23,9 @@ const GET_RECIPE = gql`
 const Recipe = () => {
   const router = useRouter();
   const { recipe_id } = router.query;
-  const { loading, error, data } = useQuery(GET_RECIPE);
+  const { loading, error, data } = useQuery(GET_RECIPE, {
+    variables: { recipeId: recipe_id },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
