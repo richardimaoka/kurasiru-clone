ea0a05feeeb9f3252d4972423d2bdf3d2889711c
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-12-18 14:41:07 +0900

    move apollo client to a separate file

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,10 +1,6 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import type { AppProps } from "next/app";
-
-const client = new ApolloClient({
-  uri: "http://localhost:8080/query",
-  cache: new InMemoryCache(),
-});
+import { client } from "../libs/apolloClient";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
