e1790a4af623a3c576efccf6a4bdf306e5f74cb0
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-12-18 21:51:58 +0900

    files dir

--- next/libs/gql/graphql.ts
+++ next/libs/gql/graphql.ts
@@ -1,16 +1,10 @@
 /* eslint-disable */
-import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>;
-};
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>;
-};
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -21,159 +15,72 @@ export type Scalars = {
 };
 
 export type Ingredient = {
-  __typename?: "Ingredient";
-  amount?: Maybe<Scalars["String"]>;
-  item?: Maybe<Scalars["String"]>;
+  __typename?: 'Ingredient';
+  amount?: Maybe<Scalars['String']>;
+  item?: Maybe<Scalars['String']>;
 };
 
 export type Ingredients = {
-  __typename?: "Ingredients";
+  __typename?: 'Ingredients';
   list?: Maybe<Array<Maybe<Ingredient>>>;
-  servings?: Maybe<Scalars["String"]>;
+  servings?: Maybe<Scalars['String']>;
 };
 
 export type Mutation = {
-  __typename?: "Mutation";
+  __typename?: 'Mutation';
   createTodo: Todo;
 };
+
 
 export type MutationCreateTodoArgs = {
   input: NewTodo;
 };
 
 export type NewTodo = {
-  text: Scalars["String"];
-  userId: Scalars["String"];
+  text: Scalars['String'];
+  userId: Scalars['String'];
 };
 
 export type Query = {
-  __typename?: "Query";
+  __typename?: 'Query';
   recipe?: Maybe<Recipe>;
   todos: Array<Todo>;
 };
 
+
 export type QueryRecipeArgs = {
-  id?: InputMaybe<Scalars["ID"]>;
+  id?: InputMaybe<Scalars['ID']>;
 };
 
 export type Recipe = {
-  __typename?: "Recipe";
-  cookingTime?: Maybe<Scalars["String"]>;
-  expense?: Maybe<Scalars["String"]>;
-  id?: Maybe<Scalars["ID"]>;
+  __typename?: 'Recipe';
+  cookingTime?: Maybe<Scalars['String']>;
+  expense?: Maybe<Scalars['String']>;
+  id?: Maybe<Scalars['ID']>;
   ingredients?: Maybe<Ingredients>;
-  introduction?: Maybe<Scalars["String"]>;
-  subTitle?: Maybe<Scalars["String"]>;
-  title?: Maybe<Scalars["String"]>;
+  introduction?: Maybe<Scalars['String']>;
+  subTitle?: Maybe<Scalars['String']>;
+  title?: Maybe<Scalars['String']>;
 };
 
 export type Todo = {
-  __typename?: "Todo";
-  done: Scalars["Boolean"];
-  id: Scalars["ID"];
-  text: Scalars["String"];
+  __typename?: 'Todo';
+  done: Scalars['Boolean'];
+  id: Scalars['ID'];
+  text: Scalars['String'];
   user: User;
 };
 
 export type User = {
-  __typename?: "User";
-  id: Scalars["ID"];
-  name: Scalars["String"];
+  __typename?: 'User';
+  id: Scalars['ID'];
+  name: Scalars['String'];
 };
 
-export type GetRecipeQueryVariables = Exact<{ [key: string]: never }>;
+export type GetRecipeQueryVariables = Exact<{ [key: string]: never; }>;
 
-export type GetRecipeQuery = {
-  __typename?: "Query";
-  recipe?: {
-    __typename?: "Recipe";
-    id?: string | null;
-    title?: string | null;
-    subTitle?: string | null;
-    introduction?: string | null;
-    cookingTime?: string | null;
-    ingredients?: {
-      __typename?: "Ingredients";
-      servings?: string | null;
-      list?: Array<{
-        __typename?: "Ingredient";
-        item?: string | null;
-        amount?: string | null;
-      } | null> | null;
-    } | null;
-  } | null;
-};
 
-export const GetRecipeDocument = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "OperationDefinition",
-      operation: "query",
-      name: { kind: "Name", value: "GetRecipe" },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          {
-            kind: "Field",
-            name: { kind: "Name", value: "recipe" },
-            arguments: [
-              {
-                kind: "Argument",
-                name: { kind: "Name", value: "id" },
-                value: {
-                  kind: "StringValue",
-                  value: "f488a1b0-9a53-4bbb-8c9c-e4e880f998ab",
-                  block: false,
-                },
-              },
-            ],
-            selectionSet: {
-              kind: "SelectionSet",
-              selections: [
-                { kind: "Field", name: { kind: "Name", value: "id" } },
-                { kind: "Field", name: { kind: "Name", value: "title" } },
-                { kind: "Field", name: { kind: "Name", value: "subTitle" } },
-                {
-                  kind: "Field",
-                  name: { kind: "Name", value: "introduction" },
-                },
-                { kind: "Field", name: { kind: "Name", value: "cookingTime" } },
-                {
-                  kind: "Field",
-                  name: { kind: "Name", value: "ingredients" },
-                  selectionSet: {
-                    kind: "SelectionSet",
-                    selections: [
-                      {
-                        kind: "Field",
-                        name: { kind: "Name", value: "servings" },
-                      },
-                      {
-                        kind: "Field",
-                        name: { kind: "Name", value: "list" },
-                        selectionSet: {
-                          kind: "SelectionSet",
-                          selections: [
-                            {
-                              kind: "Field",
-                              name: { kind: "Name", value: "item" },
-                            },
-                            {
-                              kind: "Field",
-                              name: { kind: "Name", value: "amount" },
-                            },
-                          ],
-                        },
-                      },
-                    ],
-                  },
-                },
-              ],
-            },
-          },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode<GetRecipeQuery, GetRecipeQueryVariables>;
+export type GetRecipeQuery = { __typename?: 'Query', recipe?: { __typename?: 'Recipe', id?: string | null, title?: string | null, subTitle?: string | null, introduction?: string | null, cookingTime?: string | null, ingredients?: { __typename?: 'Ingredients', servings?: string | null, list?: Array<{ __typename?: 'Ingredient', item?: string | null, amount?: string | null } | null> | null } | null } | null };
+
+
+export const GetRecipeDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetRecipe"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"recipe"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"id"},"value":{"kind":"StringValue","value":"f488a1b0-9a53-4bbb-8c9c-e4e880f998ab","block":false}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"subTitle"}},{"kind":"Field","name":{"kind":"Name","value":"introduction"}},{"kind":"Field","name":{"kind":"Name","value":"cookingTime"}},{"kind":"Field","name":{"kind":"Name","value":"ingredients"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"servings"}},{"kind":"Field","name":{"kind":"Name","value":"list"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"item"}},{"kind":"Field","name":{"kind":"Name","value":"amount"}}]}}]}}]}}]}}]} as unknown as DocumentNode<GetRecipeQuery, GetRecipeQueryVariables>;
