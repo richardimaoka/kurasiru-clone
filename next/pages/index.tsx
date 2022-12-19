6f1338aaaf9f631957dd774884f82a7d75d89b5d
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-12-19 23:48:12 +0900

    remove unused gql query fron index.ts

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,32 +1,4 @@
-import { gql, useQuery } from "@apollo/client";
-
-const GET_RECIPE = gql`
-  query GetRecipe {
-    recipe(id: "f488a1b0-9a53-4bbb-8c9c-e4e880f998ab") {
-      id
-      title
-      subTitle
-      introduction
-      cookingTime
-      ingredients {
-        servings
-        list {
-          item
-          amount
-        }
-      }
-    }
-  }
-`;
-
 const Child = () => {
-  const { loading, error, data } = useQuery(GET_RECIPE);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
-
-  console.log(data);
-
   return <div>Child</div>;
 };
 
