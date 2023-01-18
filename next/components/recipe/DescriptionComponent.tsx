b7cf858acbfba8bfcc18fe54215648d9fc507258
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-18 10:48:07 +0900

    DescriptionComponent

---
+++ next/components/recipe/DescriptionComponent.tsx
@@ -0,0 +1,70 @@
+import { css } from "@emotion/react";
+import { FragmentType, graphql, useFragment } from "../../libs/gql";
+
+export const DescriptionComponent_Fragment = graphql(`
+  fragment DescriptionComponent_Fragment on Recipe {
+    id
+    title
+    subTitle
+    introduction
+    cookingTime
+    expense
+  }
+`);
+
+interface DescriptionComponentProps {
+  fragment: FragmentType<typeof DescriptionComponent_Fragment>;
+}
+
+export const DescriptionComponent = (
+  props: DescriptionComponentProps
+): JSX.Element => {
+  const fragment = useFragment(DescriptionComponent_Fragment, props.fragment);
+  return (
+    <div
+      css={css`
+        margin-bottom: 20px;
+      `}
+    >
+      <div
+        css={css`
+          font-size: 22px;
+          font-weight: 700;
+        `}
+      >
+        {fragment.title}　レシピ・作り方
+      </div>
+      <div
+        css={css`
+          font-size: 12px;
+          margin-bottom: 20px;
+        `}
+      >
+        「{fragment.title}
+        」の作り方を簡単でわかりやすいレシピ動画で紹介しています。
+      </div>
+      <div
+        css={css`
+          margin-bottom: 20px;
+        `}
+      >
+        {fragment.introduction}
+      </div>
+      <div>調理時間：{fragment.cookingTime}</div>
+      <div>費用目安：{fragment.expense}</div>
+      <button
+        css={css`
+          width: 300px;
+          height: 50px;
+          font-size: 14px;
+          font-weight: 700;
+          background-color: f0efef;
+          border: none;
+          border-radius: 25px;
+        `}
+      >
+        保存する
+      </button>
+    </div>
+  );
+};
