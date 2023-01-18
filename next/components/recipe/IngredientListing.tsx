d6f3b8e3c3a8a67303c3ec2eac402f495d484215
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-18 10:41:28 +0900

    IngredientListing.tsx

---
+++ next/components/recipe/IngredientListing.tsx
@@ -0,0 +1,63 @@
+import { css } from "@emotion/react";
+import { FragmentType, useFragment } from "../../libs/gql";
+import { graphql } from "../../libs/gql/gql";
+import { IngredientElement } from "./IngredientElement";
+
+const IngredientListing_Fragment = graphql(`
+  fragment IngredientListing_Fragment on Ingredients {
+    servings
+    list {
+      ...IngredientElement_Fragment
+    }
+  }
+`);
+
+export interface IngredientsListingProps {
+  fragment: FragmentType<typeof IngredientListing_Fragment>;
+}
+
+export const IngredientListing = (
+  props: IngredientsListingProps
+): JSX.Element => {
+  const fragment = useFragment(IngredientListing_Fragment, props.fragment);
+  return fragment.list ? (
+    <div
+      css={css`
+        margin-top: 40px;
+      `}
+    >
+      <div
+        css={css`
+          padding-bottom: 8px;
+        `}
+      >
+        <span
+          css={css`
+            font-weight: 700;
+            font-size: 20px;
+            line-height: 20px;
+          `}
+        >
+          材料
+        </span>
+        <span
+          css={css`
+            margin-left: 8px;
+            font-size: 14px;
+          `}
+        >
+          ({fragment.servings})
+        </span>
+      </div>
+      {fragment.list.map((ingredient, index) =>
+        !ingredient ? (
+          <></>
+        ) : (
+          <IngredientElement key={index} fragment={ingredient} />
+        )
+      )}
+    </div>
+  ) : (
+    <></>
+  );
+};
